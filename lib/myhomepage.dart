ed9cd335e98588734153f678c150058992f6d556
alajemba-vik <alajemba.dev@gmail.com>
2024-09-18 02:30:57 +0000

    changing the appbar back to white

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -24,7 +24,6 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Column(
               children: [
                 Container(
-                  color: Colors.blue,
                   width: 600,
                   padding: const EdgeInsets.only(left: 3, bottom: 10.0),
                   child: Row(
